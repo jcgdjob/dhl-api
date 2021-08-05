141ea2d0eff4f88d8be1683bae611361e419a910
Jasmine <jasmine.cheng.tbs@gmail.com>
2021-08-05 12:50:00 +0800

    fixing

--- src/Entity/Base.php
+++ src/Entity/Base.php
@@ -217,19 +217,12 @@ abstract class Base extends BaseDataType
     {
         $xml = simplexml_load_string(str_replace('req:', '', $xml));
 
-
-        if ((string)$xml->Note->ActionNote != 'Success') {
-            $errorMsg = ((string)$xml->Note->Condition->ConditionCode) . ' : '
-                . ((string)$xml->Note->Condition->ConditionCode);
-            throw new \Exception('Error returned from DHL webservice : ' . $errorMsg);
-        }
-/*
         if ((string)$xml->Response->Status->Condition->ConditionCode != '') {
             $errorMsg = ((string)$xml->Response->Status->Condition->ConditionCode) . ' : '
                 . ((string)$xml->Response->Status->Condition->ConditionData);
             throw new \Exception('Error returned from DHL webservice : ' . $errorMsg);
         }
-*/
+
         $parts = explode('\\', get_class($this));
         $class_name = array_pop($parts);
         foreach ($xml->children() as $child) {
